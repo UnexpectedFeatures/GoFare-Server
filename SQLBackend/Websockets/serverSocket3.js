dcafef041c3f538ae841b5d5ce311b03da612d54
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-04-11 00:11:11 +0800

    added fetch

--- SQLBackend/Websockets/serverSocket3.js
+++ SQLBackend/Websockets/serverSocket3.js
@@ -3,15 +3,19 @@ import dotenv from "dotenv";
 import handleRegisterAdmin from "../Controllers/AdminLogic/registerController.js";
 import fetchAdmins from "../Controllers/AdminLogic/fetchAdmin.js";
 import handleDelete from "../Controllers/AdminLogic/handleDelete.js";
-
+import handleEdit from "../Controllers/AdminLogic/handleEdit.js";
+import handleSuspend from "../Controllers/AdminLogic/handleSuspend.js";
 
 dotenv.config();
 
 function startAdminSocket() {
   const port = parseInt(process.env.WS_PORT_3, 10);
   const wss = new WebSocketServer({ port });
+  console.log(`(Socket 3) WebSocket server started on port ${port}`);
 
   wss.on("connection", (ws) => {
+    fetchAdmins(ws, "[FetchAdmins]");
+
     ws.on("message", (message) => {
       const msg = message.toString();
 
@@ -21,13 +25,21 @@ function startAdminSocket() {
         handleDelete(ws, msg);
       } else if (msg.startsWith("[Suspend]")) {
         handleSuspend(ws, msg);
+      } else if (msg.startsWith("[Edit]")) {
+        handleEdit(ws, msg);
+      } else if (msg.startsWith("[FetchAdmins]")) {
+        fetchAdmins(ws, msg);
       } else {
         ws.send("[ERROR] Unknown command.");
       }
     });
 
-    ws.on("close", () => {});
-    ws.on("error", (error) => {});
+    ws.on("close", () => {
+      console.log("A client disconnected.");
+    });
+    ws.on("error", (error) => {
+      console.error("WebSocket error:", error);
+    });
   });
 }
 
