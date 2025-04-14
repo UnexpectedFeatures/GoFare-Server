6d31fdefd48bc926443b7edecb8f7b930a06c6f7
Ivan <janivantot@gmail.com>
2025-04-14 23:55:45 +0800

    firbase notif

--- Backend/Services/firebaseNotification.js
+++ Backend/Services/firebaseNotification.js
@@ -1,16 +1,21 @@
-import { onValueCreated } from "firebase-functions/v2/database";
+import { onDocumentCreated } from "firebase-functions/v2/firestore";
 import { messaging } from "../database.js";
 
-export const sendTransactionNotification = onValueCreated(
-  "/Transactions/{rfid}/{transactionId}",
+export const sendTransactionNotification = onDocumentCreated(
+  "Usertransactions/{userreferencenumber}/transactions/{transactionId}",
   async (event) => {
-    const transaction = event.data.val();
+    const transactionData = event.data?.data();
+
+    if (!transactionData) {
+      console.log("No data associated with the transaction");
+      return;
+    }
 
     try {
       const payload = {
         notification: {
           title: "New Transaction Alert",
-          body: `New transaction added: ${transaction.amount}`,
+          body: `New transaction added: ${transactionData.amount}`,
           sound: "default",
         },
       };
